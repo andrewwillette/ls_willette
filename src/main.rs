1eb1afbfa133874459ec98adff1a616667cedbcc
rjm995 <andrew.willette@capitalone.com>
2023-01-03 17:22:12 -0600

    master: adding simple test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
 use std::fs;
 
 fn main() {
+    print_current_files();
+}
+
+fn print_current_files() {
     for file in fs::read_dir(".").unwrap() {
         println!("{}", file.unwrap().file_name().to_str().unwrap());
+    }
+}
+
+fn test_me() -> &'static str {
+    return "swag";
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = test_me();
+        assert_eq!(result, "swag");
+    }
+
+    #[test]
+    fn test_print_current_files() {
+        print_current_files();
     }
 }
