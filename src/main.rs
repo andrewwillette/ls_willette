82868a132ce07d7030383d79cfcd400d5550d639
andrewwillette <willette.andrew1846@gmail.com>
2023-01-02 23:10:07 -0600

    master: printing out files in current directory

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use std::fs;
 
 fn main() {
     for file in fs::read_dir(".").unwrap() {
-        println!("{}", file.unwrap().path().display());
+        println!("{}", file.unwrap().file_name().to_str().unwrap());
     }
 }
