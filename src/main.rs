93ca55eb570cb2077602b7830ad221b1a6fb053b
andrewwillette <willette.andrew1846@gmail.com>
2023-01-12 21:20:38 -0600

    master: working on CLI arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 mod file_parsing;
+use clap::{Arg, Command};
 
 fn main() {
-    file_parsing::run_ls();
+    let matches = cli().get_matches();
+    match matches.subcommand() {
+        Some(("", _)) => {
+            println!("here");
+        }
+        _ => {
+            println!("didn't get");
+        }
+    }
+}
+
+fn cli() -> Command {
+    Command::new("ls_willette")
+        .about("ls implemented in rust")
+        .subcommand_required(false)
+        .arg(
+            Arg::new("includehidden")
+                .short('a')
+                .help("include hidden directories"),
+        )
 }
