fa022a446bb02453d61b009b89ae803918fffd40
andrewwillette <willette.andrew1846@gmail.com>
2023-01-14 03:26:46 -0600

    master: working on clap derive usage

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
 mod file_parsing;
-use clap::{Arg, Command};
+use clap::Parser;
 
 fn main() {
-    let matches = cli().get_matches();
-    match matches.subcommand() {
-        Some(("", _)) => {
-            println!("here");
-        }
-        _ => {
-            println!("didn't get");
-        }
-    }
+    let args = Cli::parse();
+    // let matches = cli().get_matches();
+    // match matches.subcommand() {
+    //     Some(("", _)) => {
+    //         println!("here");
+    //     }
+    //     _ => {
+    //         println!("didn't get");
+    //     }
+    // }
 }
 
-fn cli() -> Command {
-    Command::new("ls_willette")
-        .about("ls implemented in rust")
-        .subcommand_required(false)
-        .arg(
-            Arg::new("includehidden")
-                .short('a')
-                .help("include hidden directories"),
-        )
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short = 'f')]
+    eff: bool,
+
+    #[arg(short = 'p', value_name = "PEAR")]
+    pea: Option<String>,
+
+    #[arg(last = true)]
+    slop: Vec<String>,
 }
+
+// fn cli() -> Command {
+//     Command::new("ls_willette")
+//         .about("ls implemented in rust")
+//         .subcommand_required(false)
+//         .arg(
+//             Arg::new("includehidden")
+//                 .short('a')
+//                 .help("include hidden directories"),
+//         )
+// }
